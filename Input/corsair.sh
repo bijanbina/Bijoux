00f08d040b00c79c169792607ccd122eb3c60e5e
bijanbina <bijanbina@gmail.com>
2015-12-17 23:17:44 +0330

    Add mouse scroll script

---
+++ Input/corsair.sh
@@ -0,0 +1,1 @@
+sudo xinput set-prop 11 "Evdev Wheel Emulation Inertia" 20;sudo xinput set-prop 11 "Evdev Wheel Emulation Timeout" 1;sudo xinput set-prop 11 "Evdev Wheel Emulation Button" 9;sudo xinput set-prop 11 "Evdev Wheel Emulation" 1
