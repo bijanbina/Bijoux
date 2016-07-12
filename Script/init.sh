42716dd478fa01e9877574578ca175b5d8c0b588
bijanbina <bijanbina@gmail.com>
2016-07-12 23:46:40 +0430

    add nautilus

--- Script/init.sh
+++ Script/init.sh
@@ -1,3 +1,6 @@
 #/bin/bash
 gsettings set org.gnome.Evince allow-links-change-zoom false # make evince not change zoom
 gsettings set org.gnome.Evince.default sidebar-page links # set index as deafult sidebar
+cp ../Script/bedit ~/.local/share/nautilus/scripts
+cp ../Tweaks/scripts-accels ~/.config/nautilus/scripts-accels
+nautilus -q
