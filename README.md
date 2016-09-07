53cf6cb31f864222139790597b6a05f7aecf18e9
bijanbina <bijanbina@gmail.com>
2016-09-07 13:39:55 +0430

    add sudoers

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Bijoux
 Bijan Customized linux
+
+# New Systems
+Get started by running 
+`cd Script; sudo ./init.sh`
--- Script/init.sh
+++ Script/init.sh
@@ -4,4 +4,14 @@ gsettings set org.gnome.Evince.default sidebar-page links # set index as deafult
 cp ../Script/bedit ~/.local/share/nautilus/scripts
 cp ../Script/Terminal ~/.local/share/nautilus/scripts
 cp ../Tweaks/scripts-accels ~/.config/nautilus/scripts-accels
+echo -n "Write sudoers file [y/n]: "
+read answer
+if [ $answer == 'y' ]; then
+	cat ../Tweaks/sudoers >> /etc/sudoers
+fi
+echo -n "Install pacages [y/n]: "
+read answer
+if [ $answer == 'y' ]; then
+	pacman -S qt5-base git qt5-declerative
+fi
 nautilus -q
