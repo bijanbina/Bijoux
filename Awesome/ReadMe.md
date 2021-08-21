3373e9ef4f9a311497d96afdfd1b2e3f08dde900
bijanbina <bijanbina@gmail.com>
2021-08-21 22:36:04 +0430

    AwesomeWM: init Awesome config files

---
+++ Awesome/ReadMe.md
@@ -0,0 +1,11 @@
+## Awesome
+
+AwesomeWM configuration files
+
+```
+sudo pacman -S dmenu picom 
+git clone https://github.com/Elv13/collision
+yay polybar
+yay termite 
+xrandr --output HDMI-3 --output DVI-0 --left-of HDMI-3
+```
---
+++ Awesome/startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#sudo ~/Project/Bijoux/Script/startup_root.sh
+sudo /home/bijan/Downloads/TE/TEMP/uinput-mapper/shortcut_su
+xinput set-button-map 13 1 2 3 4 5 6 7 8 2 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+
+
+
+#picom &
+#~/.config/polybar/launch.sh &
+#xrandr --output HDMI-3 --output DVI-0 --left-of HDMI-3 &
+#/usr/lib/gsd-xsettings &
+#/usr/bin/nm-applet &
+#setxkbmap -layout \"us,ir\" -option \"grp:alt_shift_toggle\" &
