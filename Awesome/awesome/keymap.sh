5daf6d66b43687a2e709f304ef0a667f7e9012c2
bijanbina <bijanbina@gmail.com>
2021-09-10 08:47:13 +0430

    AwesomeWM: Reorganize fonts

--- Awesome/awesome/keymap.sh
+++ Awesome/awesome/keymap.sh
@@ -2,10 +2,12 @@
 
 if [[ "$1" == "8" ]];then
 
-	awesome-client 'awful = require("awful"); awful.tag.viewnext()'
+	awesome-client 'awful = require("awful"); awful.tag.viewnext(screen[1])'
+	awesome-client 'awful = require("awful"); awful.tag.viewnext(screen[2])'
 
 elif [[ "$1" == "9" ]];then
 
-	awesome-client 'awful = require("awful"); awful.tag.viewprev()'
+	awesome-client 'awful = require("awful"); awful.tag.viewprev(screen[1])'
+	awesome-client 'awful = require("awful"); awful.tag.viewprev(screen[2])'
 
 fi
