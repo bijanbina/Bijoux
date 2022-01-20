2341103fbbfbfaa9fb04fedaa2b48037949ce76c
bijanbina <bijanbina@gmail.com>
2022-01-20 10:23:24 +0330

    Polybar: Add Status to Benjamin Module

--- Awesome/polybar/awesomewm/benjamin_bar.sh
+++ Awesome/polybar/awesomewm/benjamin_bar.sh
@@ -5,19 +5,25 @@ FG_COLOR="#f3f8fa"
 X_FORMAT="%{B$BG_COLOR}%{F$FG_COLOR}"
 S_FORMAT="%{B#8800a7ff}%{F$FG_COLOR}"
 SPEX_FILE=~/.config/polybar/awesomewm/ben_spex
-SLEEP_FILE=~/.config/polybar/awesomewm/ben_sleep
+STATUS_FILE=~/.config/polybar/awesomewm/ben_status
 
 if [[ -f $SPEX_FILE ]]; then
 
 	STATUS=$(cat $SPEX_FILE)
     echo "${X_FORMAT}  SPEX ${STATUS}  %{B- F-}"
     
-elif [[ -f $SLEEP_FILE ]]; then
+elif [[ -f $STATUS_FILE ]]; then
 
-    echo "${S_FORMAT}  Sleep  %{B- F-}"
-    
+	STATUS=$(cat $STATUS_FILE)
+	
+	if [[ "$STATUS" ]]; then
+    	
+    	echo "${S_FORMAT}  ${STATUS}  %{B- F-}"
+    	
+    fi
+
 else
 
 	echo ""
-	
+
 fi
